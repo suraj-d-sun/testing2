6e757bd13fd5a10e417c071d81e5d95659d6654a
Suraj <surajdsun01@gmail.com>
2023-03-12 13:59:49 +0530

    eighth commit

---
+++ mod1/internal/ex/eg1.js
@@ -0,0 +1,13 @@
+function doSomething()
+{
+console.log("doSomething got called");
+}
+function aaa()
+{
+console.log("Looks good!");
+}
+doSomething();
+aaa();
+console.log(aaa);
+console.log(aaa());
+console.log(doSomething);
